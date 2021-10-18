6db14898130ca629f605f629b658d1f98556fbf2
vladc <vlad.chrv>
2021-10-18 15:07:54 +0200

    #2 Passing 4 lectures

--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/PharmacyDSDataProvider.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/PharmacyDSDataProvider.java
@@ -1,14 +1,22 @@
 package com.eleks.academy.pharmagator.dataproviders;
 
 import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.math.BigDecimal;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 @Service
-public class DataProviderImpl implements DataProvider {
+@RequiredArgsConstructor
+@Qualifier("pharmacyDSDataProvider")
+public class PharmacyDSDataProvider implements DataProvider {
+
+    private final WebClient dsClient;
+
     @Override
     public Stream<MedicineDto> loadData() {
         return IntStream.rangeClosed(1, 100)
---
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/ds/DSMedicinesResponse.java
@@ -0,0 +1,14 @@
+package com.eleks.academy.pharmagator.dataproviders.dto.ds;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DSMedicinesResponse {
+    private List<DSMedicineDto> products;
+}
