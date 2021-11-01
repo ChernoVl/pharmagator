d13cc3c9d5aaba49310c3848a27df8484662d20b
chernovl <vladyslav.chernov@example.com>
2021-11-01 21:44:33 +0100

    2 Added into scheduler processing multiple providers

--- api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
+++ api/src/main/java/com/eleks/academy/pharmagator/scheduler/Scheduler.java
@@ -2,8 +2,11 @@ package com.eleks.academy.pharmagator.scheduler;
 
 import com.eleks.academy.pharmagator.dataproviders.DataProvider;
 import com.eleks.academy.pharmagator.dataproviders.dto.MedicineDto;
+import com.eleks.academy.pharmagator.repositories.MedicineRepository;
+import com.eleks.academy.pharmagator.repositories.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,13 +23,25 @@ public class Scheduler {
 
     private final List<DataProvider> dataProviderList;
 
-    //@Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    private final MedicineRepository medicineRepository;
+    private final PriceRepository priceRepository;
+
+    private final ModelMapper modelMapper;
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
     public void schedule() {
         log.info("Scheduler started at {}", Instant.now());
-        dataProviderList.stream().flatMap(DataProvider::loadData).forEach(this::storeToDatabase);
+        dataProviderList.stream()
+                .flatMap(DataProvider::loadData)
+                .forEach(this::storeToDatabase);
     }
 
     private void storeToDatabase(MedicineDto dto) {
         log.info(dto.getTitle() + " - " + dto.getPrice());
+//        Price price = modelMapper.map(dto, Price.class);
+//        price.setPharmacyId(1);
+//        price.setUpdatedAt(Instant.now());
+//        medicineRepository.save(modelMapper.map(dto, Medicine.class));
+//        priceRepository.save(price);
     }
 }
