ac6f0ba44559874311dd55ddb64e5cc03ff1a951
chernovl <vladyslav.chernov@example.com>
2021-11-01 12:59:59 +0100

    Remove experimental changes

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/pharmaciess")
+@RequestMapping("/pharmacies")
 public class PharmacyController {
 
     private final PharmacyRepository pharmacyRepository;
