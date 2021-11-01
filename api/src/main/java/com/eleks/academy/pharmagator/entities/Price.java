5681ffeaf96807c237cba049c3f9acd2a1ba9924
chernovl <vladyslav.chernov@example.com>
2021-11-01 19:00:18 +0100

    1 Added validation to entities

--- api/src/main/java/com/eleks/academy/pharmagator/entities/Price.java
+++ api/src/main/java/com/eleks/academy/pharmagator/entities/Price.java
@@ -3,10 +3,7 @@ package com.eleks.academy.pharmagator.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.Instant;
 
@@ -22,5 +19,6 @@ public class Price {
     private long medicineId;
     private BigDecimal price;
     private String externalId;
+    @Column(insertable = false, updatable = false)
     private Instant updatedAt;
 }
