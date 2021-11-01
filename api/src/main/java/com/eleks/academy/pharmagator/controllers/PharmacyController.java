db42842a3bf5bc86b13534d8273438e266ce9b50
chernovl <vladyslav.chernov@example.com>
2021-11-01 12:58:25 +0100

    Experemintal change

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/pharmacies")
+@RequestMapping("/pharmaciess")
 public class PharmacyController {
 
     private final PharmacyRepository pharmacyRepository;
